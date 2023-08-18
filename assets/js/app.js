fec31a5515d757757d9331c55994ea4792276581
Jair Murillo <brook_98@live.com>
2023-08-18 10:59:57 -0600

    algunas correciones de código

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,18 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+const $n = document.querySelector('.name'); //.name
+const $b = document.querySelector('.blog');
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+async function displayUser(username) { //se agrego async
   $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  const responseJson = await fetch(baseEndpoint `${usersEndpoint}/${username}`);
+  const response = await responseJson.json(); //se paso los datos de json a object
+  console.log(response);
+  $n.textContent = `${response.name}`; //se agrego ``
+  $b.textContent = `${response.blog}`;//se agrego ``
+  $l.textContent = `${response.location}`;//se agrego ``
+  return response//return
 }
 
 function handleError(err) {
@@ -19,4 +21,4 @@ function handleError(err) {
   n.textContent = `Algo salió mal: ${err}`
 }
 
-displayUser('stolinski').catch(handleError);
+displayUser('stolinski').then().catch(handleError);
